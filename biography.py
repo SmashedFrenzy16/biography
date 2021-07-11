6c8374b644820605b922aabe48574a67ba17e88b
SmashedFrenzy16 <68993968+SmashedFrenzy16@users.noreply.github.com>
2021-07-11 14:27:44 +0100

    Create biography.py

---
+++ biography.py
@@ -0,0 +1,17 @@
+
+name = input("Enter your name: ")
+
+dob = input("Enter your Date of Birth: ")
+
+address = input("Enter your address: ")
+
+pg = input("Enter your Personal Goals: ")
+
+print("Name: {}".format(name))
+
+print("Date of Birth: {}".format(dob))
+
+print("Address: {}".format(address))
+
+print(("Personal Goals: {}").format(pg))
+
